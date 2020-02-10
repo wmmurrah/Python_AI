f0404f8db2704e72b290690e0e570b2b3ae8155a
wmmurrah <william.murrah@gmail.com>
2020-02-09 19:27:41 -0600

    Explore housing data
    Compartmentalize scripts to build flexible project

---
+++ handson-ml2/HousingProject/code/explore_housing_data.py
@@ -0,0 +1,11 @@
+# explore_housing_data.py
+"""
+Explore Housing Data
+Author: William Murrah
+Description: from chapter 2 of Hands-On ML.
+"""
+
+import matplotlib.pyplot as plt
+
+housing.hist(bins=50, figsize=(20,15))
+plt.show()
