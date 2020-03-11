259ebd1ca96a75b3b381ae7a628469ebbeb55862
wmmurrah <william.murrah@gmail.com>
2020-03-10 21:12:46 -0500

    Model Housing data
    Linear regression
    Decision tree

--- Projects/HousingProject/code/fetch_housing_data.py
+++ Projects/HousingProject/code/fetch_housing_data.py
@@ -2,12 +2,10 @@
 import os
 import tarfile
 import urllib
-import pandas as pd
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
-
 
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     """Download and unpack 1990 California Housing Prices data"""
@@ -17,6 +15,3 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
-
-fetch_housing_data()
-
