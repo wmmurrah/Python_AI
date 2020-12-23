dd52d53398487e92df0b5f86503e30d5f9fb0b6f
wmmurrah <william.murrah@gmail.com>
2020-12-23 14:15:00 -0600

    Add housing data with data.table code

---
+++ test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def print_lyrics ():
+    print("I'm a lumberjack, and I'm okay.")
+    print("I sleep all night and work all day.")
+    
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+    
+repeat_lyrics()
